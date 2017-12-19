bddb488781e349f86d5cb876777ac38f1f0e0b1b
Max <lfmax223@gmail.com>
2017-12-19 16:10:20 -0600

    Add sample external question file

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var game = {
+    // get question list from external JS file
+    questions: getQuestions(),
+    incorrectAnswers: 0,
+    correctAnswers: 0
+    
+}
+
+
+
+console.log(game.questions);
+
+
+
---
+++ assets/javascript/questions.js
@@ -0,0 +1,45 @@
+function getQuestions() { return [
+    {
+        questionText: "What does a?",
+        answers: [
+            {
+                answerText: "is a",
+                isCorrectAnswer: false
+            },
+            {
+                answerText: "is b",
+                isCorrectAnswer: true
+            },
+            {
+                answerText: "is c",
+                isCorrectAnswer: false
+            },
+            {
+                answerText: "is d",
+                isCorrectAnswer: false
+            }
+        ]
+    },
+    {
+        questionText: "What does second?",
+        answers: [
+            {
+                answerText: "is 1",
+                isCorrectAnswer: true
+            },
+            {
+                answerText: "is 3",
+                isCorrectAnswer: false
+            },
+            {
+                answerText: "is 4",
+                isCorrectAnswer: false
+            },
+            {
+                answerText: "is 2",
+                isCorrectAnswer: false
+            }
+        ]
+    }
+]
+}
