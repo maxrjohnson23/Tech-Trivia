00999f3cdfc29497f8f50b2b25654f2ad52e8b39
Max <lfmax223@gmail.com>
2017-12-19 17:44:25 -0600

    Add randomization for questions and answers

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,45 +1,128 @@
-function getQuestions() { return [
-    {
-        questionText: "What does a?",
-        answers: [
-            {
-                answerText: "is a",
-                isCorrectAnswer: false
-            },
-            {
-                answerText: "is b",
-                isCorrectAnswer: true
-            },
-            {
-                answerText: "is c",
-                isCorrectAnswer: false
-            },
-            {
-                answerText: "is d",
-                isCorrectAnswer: false
-            }
-        ]
-    },
-    {
-        questionText: "What does second?",
-        answers: [
-            {
-                answerText: "is 1",
-                isCorrectAnswer: true
-            },
-            {
-                answerText: "is 3",
-                isCorrectAnswer: false
-            },
-            {
-                answerText: "is 4",
-                isCorrectAnswer: false
-            },
-            {
-                answerText: "is 2",
-                isCorrectAnswer: false
-            }
-        ]
+function getQuestions() {
+    var questions = [
+        {
+            questionText: "Question A?",
+            answers: [
+                {
+                    answerText: "Answer 1",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 2",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Answer 3",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 4",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Question B?",
+            answers: [
+                {
+                    answerText: "Answer 1",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Answer 2",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 3",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 4",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Question C?",
+            answers: [
+                {
+                    answerText: "Answer 1",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Answer 2",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 3",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 4",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Question D?",
+            answers: [
+                {
+                    answerText: "Answer 1",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 2",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 3",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 4",
+                    isCorrectAnswer: true
+                }
+            ]
+        },
+        {
+            questionText: "Question E?",
+            answers: [
+                {
+                    answerText: "Answer 1",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 2",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Answer 3",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Answer 4",
+                    isCorrectAnswer: false
+                }
+            ]
+        }
+    ];
+
+    // Shuffle order of answers in each question
+    for(var i = 0; i < questions.length; i++) {
+        shuffleArray(questions[i].answers);
     }
-]
+
+    // Shuffle order of all the questions
+    return shuffleArray(questions);
+};
+
+// Shuffle an array usiung the Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+    for(var i = array.length - 1; i > 0; i-- ) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
