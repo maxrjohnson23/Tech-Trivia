127ed61fcd3fa98f8fa229eb72701f4f921884bf
Max <lfmax223@gmail.com>
2017-12-20 11:10:37 -0600

    Update game timing and transitions from question to answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,76 +6,92 @@ var game = {
     currentQuestionIndex: 0,
     currentTimeoutHandle: undefined,
     // 30 second timer on each question
-    timePerQuestion: 30000,
+    timePerQuestion: 5000,
     // Time to display correct answer details
     answerSummaryTime: 3000,
-    startGame: function() {
+    startGame: function () {
         this.showQuestion(this.currentQuestionIndex);
         // for (var i = 0; i < this.questions.length; i++) {
         // }
     },
-    showQuestion: function(questionIndex) {
+    showQuestion: function (questionIndex) {
         this.currentQuestionIndex = questionIndex;
         // Start timeer for each question displayed
         screenHandler.displayQuestionDetails(this.questions[questionIndex]);
-        // (function(i) {
-        //     setTimeout(function () {
-        //     }, i * 2000);
-        // })(i);
+        this.currentTimeoutHandle = setTimeout(function () {
+            game.showAnswerSummary(null)
+        }, this.timePerQuestion);
+
     },
-    checkAnswer: function(answerId) {
+    showAnswerSummary: function (answerId) {
+        console.log("Showing answer summary");
+        // remove click functions from answers
         screenHandler.disableAnswerSelection();
-        console.log("Selected answer: " + answerId);
         var currentQuestion = this.questions[this.currentQuestionIndex];
-        if(currentQuestion.answers[answerId].isCorrectAnswer) {
-            this.correctAnswers++;
+
+        // Update correct/incorrect count for game summary
+        if (answerId) {
+            currentQuestion.answers[answerId].isCorrectAnswer ? this.correctAnswers++ : this.incorrectAnswers++;
         } else {
+            // No answer selected - question timeout
             this.incorrectAnswers++;
         }
+
         // Get index of correct answer for the current question
         var correctAnswerId = currentQuestion.answers.findIndex(ans => ans.isCorrectAnswer);
         console.log("Correct answer: " + correctAnswerId);
         screenHandler.showAnswerSummary(Number(answerId), correctAnswerId);
-        this.currentTimeoutHandle = setTimeout(function() {
+        this.currentTimeoutHandle = setTimeout(function () {
             game.nextQuestion();
         }, this.answerSummaryTime);
     },
-    nextQuestion: function() {
+    nextQuestion: function () {
         console.log("On to next question");
         this.currentQuestionIndex++;
-        this.showQuestion(this.currentQuestionIndex);        
+        // If questions remain, continue game
+        if (this.currentQuestionIndex < this.questions.length) {
+            this.showQuestion(this.currentQuestionIndex);
+        } else {
+            // End game and show the summary
+            this.showGameSummary();
+        }
+    },
+    showGameSummary: function () {
+        console.log("Showing game summary");
     }
 }
 
 var screenHandler = {
     answerOrder: ["A", "B", "C", "D"],
-    displayQuestionDetails: function(question) {
+    displayQuestionDetails: function (question) {
         // Display question text
         $(".question-text").text(question.questionText);
         // Clear answers from previous questions
         var answerDiv = $(".answers").empty();
         // Display each answer
-        for(var i = 0; i < question.answers.length; i++) {
+        for (var i = 0; i < question.answers.length; i++) {
             // Format answer as "A.) Answer Text"
             var answerText = `${this.answerOrder[i]}.) ${question.answers[i].answerText}`;
+            // Add data attribute to track answer number
             var answerChoice = $("<div>").text(answerText).addClass("answer-choice").attr("data-answer", i);
             answerChoice.on("click", this.answerClicked);
             answerDiv.append(answerChoice);
         }
     },
-    answerClicked: function() {
-         // check answer ID against current question
-         var answerId = $(this).attr("data-answer");
-         game.checkAnswer(answerId); 
+    answerClicked: function () {
+        // check answer ID against current question
+        var answerId = $(this).attr("data-answer");
+        game.checkAnswer(answerId);
     },
-    showAnswerSummary: function(selectedId, correctId) {
-        if(selectedId !== correctId) {
+    showAnswerSummary: function (selectedId, correctId) {
+        if (selectedId !== correctId) {
+            // Highlight incorrect answer
             $(`.answer-choice[data-answer="${selectedId}"`).addClass("incorrect-answer");
         }
         // Highlight correct answer
         $(`.answer-choice[data-answer="${correctId}"`).addClass("correct-answer");
     },
-    disableAnswerSelection: function(){
+    disableAnswerSelection: function () {
         $(".answer-choice").off("click");
     }
 }
