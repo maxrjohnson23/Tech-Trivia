bed34e7c1487058d0b3c5a9571cfe2f13f422859
Max <lfmax223@gmail.com>
2017-12-21 09:15:31 -0600

    Add trivia questions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,110 +1,320 @@
 var questionBank = {
     questions: [
         {
-            questionText: "Question A?",
+            questionText: "What does \"HTTP\" stand for?",
             answers: [
                 {
-                    answerText: "Answer 1",
-                    isCorrectAnswer: false
-                },
-                {
-                    answerText: "Answer 2",
+                    answerText: "Hyper Text Transfer Protocol",
                     isCorrectAnswer: true
                 },
                 {
-                    answerText: "Answer 3",
+                    answerText: "Hyper Text Teletype Protocol",
                     isCorrectAnswer: false
                 },
                 {
-                    answerText: "Answer 4",
+                    answerText: "Hyper Text Type Protocol",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Hyper Text Transmission Protocol",
                     isCorrectAnswer: false
                 }
             ]
         },
         {
-            questionText: "Question B?",
+            questionText: "Who founded the WWW Consortium (W3C)?",
             answers: [
                 {
-                    answerText: "Answer 1",
+                    answerText: "Tim Berners-Lee",
                     isCorrectAnswer: true
                 },
                 {
-                    answerText: "Answer 2",
+                    answerText: "Bill Gates",
                     isCorrectAnswer: false
                 },
                 {
-                    answerText: "Answer 3",
+                    answerText: "Steve Jobs",
                     isCorrectAnswer: false
                 },
                 {
-                    answerText: "Answer 4",
+                    answerText: "Marc Andreessen",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "What is error type 403?",
+            answers: [
+                {
+                    answerText: "Site Access Forbidden/Password Protected",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Request Timeout",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Bad File Request/Syntax Error",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Page not Found",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Which collaboratively edited, free Internet encyclopedia was founded by Jimmy Wales and Larry Sanger in 2001?",
+            answers: [
+                {
+                    answerText: "Wikipedia",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "StumbleUpon",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Mashable",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "About.com",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "What does \"LCD\" stand for?",
+            answers: [
+                {
+                    answerText: "Long Connection Display",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Line Control Display",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Liquid Crystal Display",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Local Connection Display",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Which commerial music streaming service was founded in Sweden in 2006?",
+            answers: [
+                {
+                    answerText: "Spotify",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Last.fm",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "The Pirate Bay",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Pandora Radio",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "How many bits are in a byte?",
+            answers: [
+                {
+                    answerText: "8",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "1",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "4",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "16",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Which programming language was created by James Gosling at Sun Microsystems in 1995?",
+            answers: [
+                {
+                    answerText: "Java",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Perl",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Python",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "PHP",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "TCP port number 80 is usually reserved for?",
+            answers: [
+                {
+                    answerText: "HTTP",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "FTP",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Telnet",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Email",
+                    isCorrectAnswer: false
+                }
+            ]
+        }, 
+        {
+            questionText: "In which year was the Personal Computer featured as Time's \"Person of the Year\"?",
+            answers: [
+                {
+                    answerText: "1982",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "1991",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "1995",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "1987",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "The Microsoft Corporation moved its world headquarters to which location in 1986?",
+            answers: [
+                {
+                    answerText: "Redmond, Washington",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Cupertino, California",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Seattle, Washington",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "San Francisco, California",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "The Linux operating system was developed by which University of Helsinki software engineer?",
+            answers: [
+                {
+                    answerText: "Linus Torvalds",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Carolus Linnaeus",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Linus Pauling",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Linus Van Pelt",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Which computer CEO started his multi-billion dollar business in his Univeristy of Texas dorm room?",
+            answers: [
+                {
+                    answerText: "Michael Dell",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Bill Gates",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Steve Jobs",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Larry Page",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "ReactJS is a JavaScript framework developed by which company?",
+            answers: [
+                {
+                    answerText: "Facebook",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Google",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Amazon",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Airbnb",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            questionText: "Who is the creator of JavaScript?",
+            answers: [
+                {
+                    answerText: "Brendan Eich",
+                    isCorrectAnswer: true
+                },
+                {
+                    answerText: "Bjarne Stroustrup",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Guido van Rossum",
+                    isCorrectAnswer: false
+                },
+                {
+                    answerText: "Kent Beck",
                     isCorrectAnswer: false
                 }
             ]
         }
-        // {
-        //     questionText: "Question C?",
-        //     answers: [
-        //         {
-        //             answerText: "Answer 1",
-        //             isCorrectAnswer: true
-        //         },
-        //         {
-        //             answerText: "Answer 2",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 3",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 4",
-        //             isCorrectAnswer: false
-        //         }
-        //     ]
-        // },
-        // {
-        //     questionText: "Question D?",
-        //     answers: [
-        //         {
-        //             answerText: "Answer 1",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 2",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 3",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 4",
-        //             isCorrectAnswer: true
-        //         }
-        //     ]
-        // },
-        // {
-        //     questionText: "Question E?",
-        //     answers: [
-        //         {
-        //             answerText: "Answer 1",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 2",
-        //             isCorrectAnswer: false
-        //         },
-        //         {
-        //             answerText: "Answer 3",
-        //             isCorrectAnswer: true
-        //         },
-        //         {
-        //             answerText: "Answer 4",
-        //             isCorrectAnswer: false
-        //         }
-        //     ]
-        // }
     ],
     getQuestions: function () {
         // Shuffle order of answers in each question
@@ -126,14 +336,3 @@ var questionBank = {
 
 
 };
-
-// // Shuffle an array usiung the Durstenfeld shuffle algorithm
-// function shuffleArray(array) {
-//     for (var i = array.length - 1; i > 0; i--) {
-//         var j = Math.floor(Math.random() * (i + 1));
-//         var temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-//     }
-//     return array;
-// }
